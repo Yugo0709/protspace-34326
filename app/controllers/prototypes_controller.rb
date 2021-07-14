f1bbcd7d2c6a1df5205840b6412a9659e87e2794
Yugo0709 <79342552+Yugo0709@users.noreply.github.com>
2021-07-14 20:00:32 +0900

    modify

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
-  protect_from_forgery
+
   
   def index
     @prototypes = Prototype.includes(:user)
